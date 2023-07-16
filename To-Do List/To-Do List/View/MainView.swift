d06396fbf8cf4bf4e2e06f7b088b254753a28ef5
AFFLatusss <jasonyee1999@gmail.com>
2023-07-16 16:39:36 +0100

    started dev

--- To-Do List/To-Do List/View/MainView.swift
+++ To-Do List/To-Do List/View/MainView.swift
@@ -9,16 +9,12 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            LogInView()
         }
-        .padding()
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
